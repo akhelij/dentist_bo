88aaefba54313e804bf18dbab3729cc6d92d0f5a
Mohamed Akhelij <m.akhelij@gmail.com>
2022-10-23 23:16:42 +0100

    adding Demo seeder

---
+++ database/seeders/DemoSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Tenant;
+use App\Models\User;
+
+class DemoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $tenants = Tenant::factory(3)->create();
+ 
+        $admin = User::factory()->create([
+            'tenant_id' => null,
+            'email' => 'admin@admin.com',
+        ]);       
+        
+        foreach($tenants as $tenant) {      
+            auth()->loginUsingId($admin->id);
+            session()->put('tenant_id', $tenant->id);
+
+            $doctor = User::factory()->create([
+                'role' => 'doctor',
+            ]);   
+            
+            auth()->loginUsingId($doctor->id);
+            
+            User::factory(2)->create([
+                'role' => 'staff'
+            ]);
+        }
+
+        auth()->loginUsingId($admin->id);
+    }
+}
+
