308550e54ac0ccbe02486782298de028753edd09
Mohamed Akhelij <m.akhelij@gmail.com>
2022-10-28 09:50:06 +0100

    perf: [CLIENT] [MIGRATION] add client migration

---
+++ database/migrations/2022_10_28_082316_create_clients_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->date('birthdate')->nullable();
+            $table->string('job')->nullable();
+            $table->text('address')->nullable();
+            $table->string('phone')->nullable();
+            $table->text('notes')->nullable();
+            $table->unsignedBigInteger('tenant_id')->index();
+            $table->unsignedBigInteger('created_by')->nullable();
+            $table->unsignedBigInteger('updated_by')->nullable();
+            $table->unsignedBigInteger('deleted_by')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+};
