160f652755ffa5045e074e93b8e2dd63adf3edc7
Mohamed Akhelij <m.akhelij@gmail.com>
2022-10-29 13:24:00 +0100

    perf: [INTERVENTIONS] [MIGRATION]

---
+++ database/migrations/2022_10_29_091945_create_interventions_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('interventions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('patient_id')->constrained();
+            $table->json('dents')->nullable();
+            $table->string('description')->nullable();
+            $table->float('total_amount')->nullable();
+            $table->unsignedBigInteger('tenant_id')->index();
+            $table->unsignedBigInteger('created_by')->nullable();
+            $table->unsignedBigInteger('updated_by')->nullable();
+            $table->unsignedBigInteger('deleted_by')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('interventions');
+    }
+};
