b5d615bfd4810ff9b6436c3a40f5be4b497da863
Mohamed Akhelij <m.akhelij@gmail.com>
2022-12-11 13:39:21 +0100

    fix: [Dashboard] Provide needed data to be displayed in dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\InterventionRequest;
+use App\Models\Appointment;
 use App\Models\Intervention;
 use App\Models\Patient;
+use App\Models\Payment;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
@@ -16,9 +20,17 @@ class DashboardController extends Controller
      */
     public function index()
     {
+        $result = Payment::select(DB::raw('MONTH(created_at) as labels, SUM(amount) as data'))->whereBetween('created_at', [now()->subYears(1), now()])->groupBy('labels')->get();
+        $chart['labels'] = $result->pluck('labels');
+        $chart['data'] = $result->pluck('data');
+
         return [
             'patients_count' => Patient::count(),
-            'interventions_count' => Intervention::count()
+            'interventions_count' => Intervention::count(),
+            'today_appointments' => Appointment::whereDate('date', today())->get(),
+            'appointments_count' => Appointment::count(),
+            'payments_total' => Payment::all()->sum('amount'),
+            'chart' => $chart
         ];
     }
 
